dbb72952c9b3a2c503309d36468c910d8fd82820
Marcin Bojarski <boj.marcin@gmail.com>
2018-02-27 13:58:24 +0100

    Add scripts to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var a = prompt(' Enter the length of the triangle base ');
+var h = prompt(' Enter the height of the triangle ');
+var triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+alert('The triangle field is: ' + triangleArea);
